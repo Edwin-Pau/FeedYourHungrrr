b9190a5844538ffe8e6af1047e0b07860717505b
Edwin Pau <pau.edwin@live.ca>
2021-04-11 01:46:38 -0600

    Fix CORS origin
    Signed-off-by: Edwin Pau <pau.edwin@live.ca>

--- src/app.js
+++ src/app.js
@@ -12,8 +12,11 @@ console.log(__dirname)
 // Initializes express and sets up the paths.
 const app = express()
 
-app.use(cors());
-app.disable('etag');
+var corsOptions = {
+    origin: 'https://lucaswgong.com',
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 
 // Customizes server, automatically parse incoming json into an object
 app.use(express.json())
