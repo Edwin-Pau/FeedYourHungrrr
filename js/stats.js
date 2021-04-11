02d6b2bacbc3c67f0cf7364ded0c68fac5f5ee77
Edwin Pau <pau.edwin@live.ca>
2021-04-11 03:48:21 -0600

    Fix navigation menu
    Signed-off-by: Edwin Pau <pau.edwin@live.ca>

--- js/stats.js
+++ js/stats.js
@@ -1,11 +1,11 @@
-function getStats(){
+function getStats() {
     return new Promise((resolve, reject) => {
         let xhttp = new XMLHttpRequest();
         xhttp.open("GET", "https://feedyourhungrrr.herokuapp.com/api/v1/stats", true)
         xhttp.send();
         xhttp.onreadystatechange = function () {
-            if (this.readyState === 4 && this.status === 200){
-                if (this.responseText){
+            if (this.readyState === 4 && this.status === 200) {
+                if (this.responseText) {
                     resolve(this.responseText);
                 } else {
                     reject(new Error("Request is failed"));
@@ -15,10 +15,20 @@ function getStats(){
     });
 }
 
-if (!localStorage.getItem(token)) {
-
+if (!localStorage.getItem("token")) {
+    document.getElementById("num_get_rest").innerHTML = "Must be authenticated to see stats!"
+    document.getElementById("num_get_rest2").innerHTML = "Must be authenticated to see stats!"
+    document.getElementById("num_post_rest").innerHTML = "Must be authenticated to see stats!"
+    document.getElementById("num_put_rest").innerHTML = "Must be authenticated to see stats!"
+    document.getElementById("num_delete_rest").innerHTML = "Must be authenticated to see stats!"
+    document.getElementById("num_get_menu").innerHTML = "Must be authenticated to see stats!"
+    document.getElementById("num_post_menu").innerHTML = "Must be authenticated to see stats!"
+    document.getElementById("num_put_menu").innerHTML = "Must be authenticated to see stats!"
+    document.getElementById("num_delete_menu").innerHTML = "Must be authenticated to see stats!"
+    document.getElementById("num_post_signup").innerHTML = "Must be authenticated to see stats!"
+    document.getElementById("num_post_login").innerHTML = "Must be authenticated to see stats!"
 } else {
-    getStats().then(function(data) {
+    getStats().then(function (data) {
         console.log("stat data");
         console.log(data);
         let statsArr = JSON.parse(data);
@@ -33,8 +43,8 @@ if (!localStorage.getItem(token)) {
         document.getElementById("num_delete_menu").innerHTML = statsArr[8].StatUsage;
         document.getElementById("num_post_signup").innerHTML = statsArr[9].StatUsage;
         document.getElementById("num_post_login").innerHTML = statsArr[10].StatUsage;
-    
-    }).catch(function(err) {
+
+    }).catch(function (err) {
         console.error(err);
     });
 }
